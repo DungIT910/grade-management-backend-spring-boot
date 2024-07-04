514249a817e9c6bcbee03e7cb4cf5caaaf97ce04
DungIT910 <Dungtb611@gmail.com>
2024-07-04 10:01:14 +0700

    chore: init project

---
+++ src/main/java/com/boolfly/GradeManagementRestful/GradeManagementRestfulApplication.java
@@ -0,0 +1,13 @@
+package com.boolfly.GradeManagementRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeManagementRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeManagementRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boolfly/GradeManagementRestful/GradeManagementRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boolfly.GradeManagementRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeManagementRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
